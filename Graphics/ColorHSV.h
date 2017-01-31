58545086c3c6864064feffcb9b2e62e3bd10fff0
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2017-01-31 16:59:14 +0900

    Colorを追加．

---
+++ Graphics/ColorHSV.h
@@ -0,0 +1,52 @@
+#ifndef __CRYSTAL_GRAPHICS_COLOR_HSV_H__
+#define __CRYSTAL_GRAPHICS_COLOR_HSV_H__
+
+#include "ColorRGBA.h"
+#include <cassert>
+
+namespace Crystal {
+	namespace Graphics {
+		template<typename>
+		class ColorRGBA;
+
+		class ColorHSV
+		{
+		public:
+
+			ColorHSV();
+
+			ColorHSV(const float h, const float s, const float v);
+
+			float getH() const { return h; }
+
+			float getS() const { return s; }
+
+			float getV() const { return v; }
+
+			void setH(const float h) { this->h = h; }
+
+			void setS(const float s) { this->s = s; }
+
+			void setV(const float v) { this->v = v; }
+
+			bool equals(const ColorHSV& rhs) const;
+
+			bool operator==(const ColorHSV& rhs) const {
+				return equals(rhs);
+			}
+
+			bool operator!=(const ColorHSV& rhs) const {
+				return !equals(rhs);
+			}
+
+			ColorRGBA<float> toColorRGBA() const;
+
+		private:
+			float h;
+			float s;
+			float v;
+		};
+	}
+}
+
+#endif
---
+++ Graphics/ColorRGBA.cpp
@@ -0,0 +1,98 @@
+#include "ColorRGBA.h"
+#include "ColorHSV.h"
+
+#include <algorithm>
+
+using namespace Crystal::Graphics;
+
+template<typename T>
+ColorRGBA<T>::ColorRGBA() :
+	red(0),
+	green(0),
+	blue(0),
+	alpha(0)
+{}
+
+
+
+template<typename T>
+ColorRGBA<T>::ColorRGBA(const T red, const T green, const T blue, const T alpha = 255) :
+	red(red),
+	green(green),
+	blue(blue),
+	alpha(alpha)
+{
+}
+
+
+template<typename T>
+std::array<T, 3> ColorRGBA<T>::toArray3() const
+{
+	std::array<T, 3> results;
+	results[0] = getRed();
+	results[1] = getGreen();
+	results[2] = getBlue();
+	return results;
+}
+
+template<typename T>
+std::vector<T> ColorRGBA<T>::toArray4() const
+{
+	std::vector<T> results(4);
+	results[0] = getRed();
+	results[1] = getGreen();
+	results[2] = getBlue();
+	results[3] = getAlpha();
+	return results;
+}
+
+template<typename T>
+bool ColorRGBA<T>::equals(const ColorRGBA& rhs) const
+{
+	return
+		(getRed() == rhs.getRed()) &&
+		(getGreen() == rhs.getGreen()) &&
+		(getBlue() == rhs.getBlue()) &&
+		(alpha == rhs.getAlpha());
+}
+
+/*
+template<typename T>
+ColorHSV ColorRGBA<T>::toHSV() const
+{
+	const auto r = red / T{ 255 };
+	const auto g = green / T{ 255 };
+	const auto b = blue / T{ 255 };
+
+	const auto min = std::min<T>(r, std::min<T>(g, b));
+	const auto max = std::max<T>(r, std::max<T>(g, b));
+
+	const auto v = max;
+	const auto s = max - min;
+
+	if (min == max) {
+		const T h = T{ 0 };
+		return ColorHSV(h, s, v);
+	}
+	else if (min == b) {
+		const T h = T{ 60 } *(g - r) / (max - min) + T{ 60 };
+		return ColorHSV(h, s, v);
+	}
+	else if (min == r) {
+		const auto h = T{ 60 } *(b - g) / (max - min) + T{ 180 };
+		return ColorHSV(h, s, v);
+	}
+	else if (min == g) {
+		const auto h = T{ 60 } *(r - b) / (max - min) + T{ 300 };
+		return ColorHSV(h, s, v);
+	}
+	else {
+		assert(false);
+		return ColorHSV();
+	}
+}
+*/
+
+//template class ColorRGBA<unsigned char>;
+template class ColorRGBA<float>;
+template class ColorRGBA<double>;
