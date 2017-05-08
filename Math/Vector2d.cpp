78cba0c9d45160388429c60f7e381e95991f7a2a
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2017-05-08 13:21:54 +0900

    2次元クラスを補強．

---
+++ Math/Vector2d.cpp
@@ -0,0 +1,117 @@
+#include "Vector2d.h"
+
+using namespace Crystal::Math;
+
+template<typename T>
+Vector2d<T>::Vector2d()
+	: Vector2d(0, 0)
+{}
+
+template<typename T>
+Vector2d<T>::Vector2d(const T x, const T y) :
+	x(x), y(y)
+{}
+
+template<typename T>
+Vector2d<T>& Vector2d<T>::scale(const T scale)
+{
+	x *= scale;
+	y *= scale;
+	return *this;
+}
+
+template<typename T>
+Vector2d<T> Vector2d<T>::scaled(const T scale) const
+{
+	return Vector2d<T>(x*scale, y*scale);
+}
+
+template<typename T>
+T Vector2d<T>::getDistanceSquared(const Vector2d& rhs) const
+{
+	return pow(x - rhs.x, 2) + pow(y - rhs.y, 2);
+}
+
+template<typename T>
+T Vector2d<T>::getLengthSquared() const
+{
+	return x*x + y*y;
+}
+
+template<typename T>
+T Vector2d<T>::getLength() const
+{
+	return std::sqrt(getLengthSquared());
+}
+
+template<typename T>
+bool Vector2d<T>::equals(const Vector2d& rhs) const
+{
+	return
+		Tolerance<T>::isEqualLoosely(this->x, rhs.x) &&
+		Tolerance<T>::isEqualLoosely(this->y, rhs.y);
+}
+
+/*
+template<typename T>
+Angle<T> Vector2d<T>::getAngle(const Vector2d<T>& rhs) const
+{
+	return Angle<T>(Radian<T>(std::acos(getInnerProduct(rhs))));
+}
+
+template<typename T>
+Angle<T> Vector2d<T>::getSignedAngle(const Vector2d<T>& rhs) const
+{
+	const auto angle = getAngle(rhs);
+	return isLeft(rhs) ? angle : -angle;
+}
+*/
+
+template<typename T>
+T Vector2d<T>::getInnerProduct(const Vector2d<T>& rhs) const
+{
+	return x*rhs.x + y * rhs.y;
+}
+
+template<typename T>
+Vector2d<T> Vector2d<T>::normalize()
+{
+	const auto length = getLength();
+	x /= length;
+	y /= length;
+	return *this;
+}
+
+template<typename T>
+Vector2d<T> Vector2d<T>::normalized() const
+{
+	Vector2d<T> vector = *(this);
+	return vector.normalize();
+}
+
+/*
+template<typename T>
+Angle<T> Vector2d<T>::getAngle() const
+{
+	return Angle<T>(Radian<T>(::atan2(y, x)));
+}
+*/
+
+template<typename T>
+Vector2d<T>& Vector2d<T>::operator+=(const Vector2d& rhs)
+{
+	this->x += rhs.x;
+	this->y += rhs.y;
+	return *this;
+}
+
+template<typename T>
+Vector2d<T>& Vector2d<T>::operator-=(const Vector2d& rhs)
+{
+	this->x -= rhs.x;
+	this->y -= rhs.y;
+	return *this;
+}
+
+template class Vector2d<float>;
+template class Vector2d<double>;
---
+++ Shader/OnScreenRenderer.cpp
@@ -0,0 +1,84 @@
+#include "OnScreenRenderer.h"
+
+#include "../Math/Vector2d.h"
+#include "../Math/Box2d.h"
+
+#include <sstream>
+
+using namespace Crystal::Math;
+using namespace Crystal::Graphics;
+using namespace Crystal::Shader;
+
+bool OnScreenRenderer::build()
+{
+	const auto vsSource = getBuildinVertexShaderSource();
+	const auto fsSource = getBuildinFragmentShaderSource();
+	bool b = shader.build(vsSource, fsSource);
+	findLocation();
+	return b;
+}
+
+std::string OnScreenRenderer::getBuildinVertexShaderSource()
+{
+	std::ostringstream stream;
+	stream
+		<< "#version 150" << std::endl
+		<< "in vec2 position;" << std::endl
+		<< "out vec2 texCoord;" << std::endl
+		<< "void main(void) {" << std::endl
+		<< "	texCoord = (position + vec2(1.0,1.0))/2.0;" << std::endl
+		<< "	gl_Position = vec4(position, 0.0, 1.0);" << std::endl
+		<< "}" << std::endl;
+	ShaderUnit vertexShader;
+	bool b = vertexShader.compile(stream.str(), ShaderUnit::Stage::VERTEX);
+	return stream.str();
+}
+
+std::string OnScreenRenderer::getBuildinFragmentShaderSource()
+{
+	std::ostringstream stream;
+	stream
+		<< "#version 150" << std::endl
+		<< "uniform sampler2D texture;" << std::endl
+		<< "in vec2 texCoord;" << std::endl
+		<< "out vec4 fragColor;" << std::endl
+		<< "void main(void) {" << std::endl
+		<< "	fragColor = texture2D(texture, texCoord);" << std::endl
+		<< "}" << std::endl;
+	ShaderUnit fragmentShader;
+	bool b = fragmentShader.compile(stream.str(), ShaderUnit::Stage::FRAGMENT);
+	return stream.str();
+}
+
+void OnScreenRenderer::findLocation()
+{
+	shader.findUniformLocation("texture");
+	shader.findAttribLocation("position");
+}
+
+void OnScreenRenderer::render(const ITextureObject& texture)
+{
+	const Box2d<float> box(Vector2d<float>(-1.0, -1.0), Vector2d<float>(1.0, 1.0));
+	const auto& positions = box.toArray();
+
+	//glEnable(GL_DEPTH_TEST);
+
+	glUseProgram(shader.getId());
+
+	texture.bind();
+
+	glUniform1i(shader.getUniformLocation("texture"), texture.getId());
+
+	glVertexAttribPointer(shader.getAttribLocation("positions"), 2, GL_FLOAT, GL_FALSE, 0, positions.data());
+
+	glEnableVertexAttribArray(0);
+	glDrawArrays(GL_QUADS, 0, static_cast<GLsizei>(positions.size() / 2));
+	glDisableVertexAttribArray(0);
+
+	glBindFragDataLocation(shader.getId(), 0, "fragColor");
+
+	texture.unbind();
+	//glDisable(GL_DEPTH_TEST);
+
+	glUseProgram(0);
+}
