d14fcdf0895be1edc45b7c3ebf527d8dd18db9db
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2017-06-08 16:36:19 +0900

    抽象化．

--- Sample/CameraNode.cpp
+++ Sample/CameraNode.cpp
@@ -8,29 +8,34 @@ namespace {
 	static inline ImVec2 operator-(const ImVec2& lhs, const ImVec2& rhs) { return ImVec2(lhs.x - rhs.x, lhs.y - rhs.y); }
 }
 
-void Matrix4dSlot::show(const ImVec2& pos, ImDrawList* drawList)
+void IInSlot::show(const ImVec2& pos, ImDrawList* drawList)
+{
+	const float NODE_SLOT_RADIUS = 4.0f;
+	drawList->AddCircleFilled(pos, NODE_SLOT_RADIUS, ImColor(150, 150, 150, 150));
+}
+
+void IOutSlot::show(const ImVec2& pos, ImDrawList* drawList)
 {
 	const float NODE_SLOT_RADIUS = 4.0f;
 	drawList->AddCircleFilled(pos, NODE_SLOT_RADIUS, ImColor(150, 150, 150, 150));
 }
 
 CameraNode::CameraNode(const int id, const std::string& name, const ImVec2& pos) :
-	id(id),
-	name(name),
-	pos(pos)
+	INode(id, name, pos)
 {}
-
 
 void CameraNode::show(ImDrawList* drawList, ImVec2 offset)
 {
-	drawList->AddRectFilled(pos + offset, pos + offset + ImVec2(100, 100), ImColor(60, 60, 60), 4.0f);
+	ImGui::PushID(this->getId());
 
-	projectionMatrixSlot.show(offset + pos + ImVec2(100,0), drawList);
-	modelviewMatrixSlot.show(offset + pos + ImVec2(100, 10), drawList);
+	drawList->AddRectFilled(getPosition() + offset, getPosition() + offset + ImVec2(100, 100), ImColor(60, 60, 60), 4.0f);
+
+	projectionMatrixSlot.show(offset + getPosition() + ImVec2(100,0), drawList);
+	modelviewMatrixSlot.show(offset + getPosition() + ImVec2(100, 10), drawList);
 	//drawList->AddCircleFilled(offset + ImVec2(10, 10), 10, ImColor(255, 100, 100, 100));
 
+	ImGui::PopID();
 	/*
-	ImGui::PushID(this->id);
 
 	ImGui::Button(name.c_str(), ImVec2(100, 100));
 	static float eyePosition[3];
--- Sample/CameraNode.h
+++ Sample/CameraNode.h
@@ -12,31 +12,100 @@ namespace Crystal {
 	namespace UI {
 		class ShaderOutputSlot;
 
-class Matrix4dSlot
+
+class IInSlot
 {
 public:
-
-	void show(const ImVec2& pos,ImDrawList* drawList);
-
-private:
-	Math::Matrix4d<float> value;
+	virtual ~IInSlot() {}
+	virtual void show(const ImVec2& pos, ImDrawList* drawList);
 };
 
-class CameraNode
+class Matrix4dInSlot : public IInSlot
+{
+public:
+	void setValue(const std::array<double, 16>& value) { this->value = value; }
+
+private:
+	std::array<double, 16> value;
+};
+
+
+class IOutSlot
+{
+public:
+	virtual ~IOutSlot() {}
+
+	virtual void show(const ImVec2& pos, ImDrawList* drawList);
+};
+
+class Matrix4dOutSlot : public IOutSlot
+{
+public:
+	~Matrix4dOutSlot()
+	{}
+
+
+	Math::Matrix4d<double> getValue() const { return value; }
+
+private:
+	Math::Matrix4d<double> value;
+};
+
+class ILink
+{
+public:
+	virtual void build() = 0;
+};
+
+class Matrix4dLink : public ILink
+{
+private:
+	void build() override {
+		input->setValue( output->getValue().toArray() );
+	}
+
+	Matrix4dOutSlot* output;
+	Matrix4dInSlot* input;
+};
+
+class INode
+{
+public:
+	INode(const int id, const std::string& name, const ImVec2& pos) :
+		id(id),
+		name(name),
+		position(pos)
+	{}
+
+	virtual ~INode() {}
+
+	const int getId() const { return id; }
+
+	const std::string getName() const { return name; }
+
+	const ImVec2 getPosition() const { return position; }
+
+private:
+	const int id;
+	const std::string name;
+	const ImVec2 position;
+
+	std::vector<IInSlot*> inputs;
+	std::vector<IOutSlot*> outputs;
+};
+
+class CameraNode : public INode
 {
 public:
 	CameraNode(const int id, const std::string& name, const ImVec2& pos);
 
 	void show(ImDrawList* drawList, ImVec2 offset);
 
+	//void build();
+
 private:
-	const int id;
-	const std::string name;
-	const ImVec2 pos;
-	//std::vector<ShaderOutputSlot*> outputSlots;
-	Matrix4dSlot projectionMatrixSlot;
-	Matrix4dSlot modelviewMatrixSlot;
-//	Matrix4dSlot 
+	Matrix4dOutSlot projectionMatrixSlot;
+	Matrix4dOutSlot modelviewMatrixSlot;
 	Graphics::PerspectiveCamera<float> camera;
 };
 
