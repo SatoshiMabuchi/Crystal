222b1a50788b8e8f3a763e81e15c3f57bb7711fd
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2017-11-06 11:43:16 +0900

    add ui input.

--- AppBase/ParticlePanel.cpp
+++ AppBase/ParticlePanel.cpp
@@ -17,12 +17,23 @@ namespace {
 			canvas(canvas)
 		{}
 
+		struct Args {
+			Box3d<float> box;
+			glm::vec3 dx;
+		};
+
+		void setArgs(Args args) {
+			this->args = args;
+		};
+
 		void execute() override
 		{
 			std::vector<Vector3d<float>> positions;
-			for (double x = -5.0; x < 5.0; x += 1.0) {
-				for (double y = -5.0; y < 5.0; y += 1.0) {
-					for (double z = -5.0; z < 5.0; z += 1.0) {
+			const auto& min = this->args.box.getMin();
+			const auto& max = this->args.box.getMax();
+			for (double x = min.getX(); x < max.getX(); x += args.dx[0]) {
+				for (double y = min.getY(); y < max.getY(); y += args.dx[1]) {
+					for (double z = min.getZ(); z < max.getZ(); z += args.dx[2]) {
 						positions.push_back(Vector3d<float>(x, y, z));
 					}
 				}
@@ -32,9 +43,11 @@ namespace {
 		}
 
 	private:
+		Args args;
 		IModel* model;
 		ICanvas* canvas;
 	};
+
 }
 
 void ParticlePanel::show()
@@ -43,8 +56,19 @@ void ParticlePanel::show()
 		ImGui::OpenPopup("Box");
 	}
 	if (ImGui::BeginPopup("Box")) {
+		float min[3] = { -10.0f, -10.0f, -10.0f };
+		ImGui::InputFloat3("Min", min);
+		float max[3] = { 10.0f, 10.0f, 10.0f };
+		ImGui::InputFloat3("Max", max);
+		glm::vec3 dx = { 1.0f, 1.0f, 1.0f };
+		ImGui::InputFloat3("Dx", &dx[0]);
+
 		if (ImGui::Button("OK")) {
 			ParticleGenerateCommand command(model, canvas);
+			ParticleGenerateCommand::Args args;
+			args.box = Box3d<float>(Vector3d<float>(min[0], min[1], min[2]), Vector3d<float>(max[0], max[1], max[2]));
+			args.dx = dx;
+			command.setArgs(args);
 			command.execute();
 			ImGui::CloseCurrentPopup();
 		}
