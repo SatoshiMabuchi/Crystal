6d38b951ee01c9486747a38b9c2fffa9e0bdf449
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2017-11-06 16:29:36 +0900

    add helper function for glm.

--- Math/Vector3d.h
+++ Math/Vector3d.h
@@ -13,6 +13,27 @@ namespace Crystal {
 using Vector3df = glm::vec3;
 using Vector3dd = glm::dvec3;
 
+static float getLengthSquared(const Vector3df& v)
+{
+	return std::pow(v.x, 2.0f) + std::pow(v.y, 2.0f) + std::pow(v.z, 2.0f);
+}
+
+static double getDistanceSquared(const Vector3dd& v)
+{
+	return std::pow(v.x, 2.0) + std::pow(v.y, 2.0) + std::pow(v.z, 2.0);
+}
+
+static float getDistanceSquared(const Vector3df& lhs, const Vector3df& rhs)
+{
+	return std::pow(lhs.x - rhs.x, 2.0f) + std::pow(lhs.y - rhs.y, 2.0f) + std::pow(lhs.z - rhs.z, 2.0f);
+}
+
+static double getDistanceSquared(const Vector3dd& lhs, const Vector3dd& rhs)
+{
+	return std::pow(lhs.x - rhs.x, 2.0) + std::pow(lhs.y - rhs.y, 2.0) + std::pow(lhs.z - rhs.z, 2.0);
+}
+
+
 	}
 }
 
