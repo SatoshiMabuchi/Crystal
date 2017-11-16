49d8d6d29ee9a660bb934f5a9d381c003036534f
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2017-11-16 19:06:02 +0900

    delete .cpp file.

--- Math/Box2d.h
+++ Math/Box2d.h
@@ -7,65 +7,124 @@
 namespace Crystal {
 	namespace Math {
 
-		class Box2d
-		{
-		public:
-			Box2d();
+class Box2d
+{
+public:
+	Box2d() :
+		Box2d(Vector2df(0, 0), Vector2df(1, 1))
+	{}
 
-			Box2d(const Vector2df& pointX, const Vector2df& pointY);
+	Box2d(const Vector2df& pointX, const Vector2df& pointY) {
+		const auto x = std::min<float>(pointX.x, pointY.x);
+		const auto y = std::min<float>(pointX.y, pointY.y);
+		this->start = Vector2df(x, y);
+		const auto endX = std::max<float>(pointX.x, pointY.x);
+		const auto endY = std::max<float>(pointX.y, pointY.y);
+		this->end = Vector2df(endX, endY);
+		assert(isValid());
+	}
 
-			void add(const Vector2df& v);
 
-			void add(const Box2d& b);
+	void add(const Vector2df& v) {
+		const auto x = std::min<float>(getMinX(), v.x);
+		const auto y = std::min<float>(getMinY(), v.y);
+		start = Vector2df(x, y);
 
-			float getArea() const;
+		const auto endX = std::max<float>(end.x, v.x);
+		const auto endY = std::max<float>(end.y, v.y);
+		end = Vector2df(endX, endY);
+	}
 
-			Vector2df getMax() const { return Vector2df(end[0], end[1]); }
+	void add(const Box2d& b) {
+		const auto sx = std::min<float>(getMinX(), b.getMinX());
+		const auto sy = std::min<float>(getMinY(), b.getMinY());
+		this->start = Vector2df(sx, sy);
 
-			Vector2df getMin() const { return Vector2df(getMinX(), getMinY()); }
+		const auto ex = std::max<float>(end.x, b.getMaxX());
+		const auto ey = std::max<float>(end.y, b.getMaxY());
+		this->end = Vector2df(ex, ey);
+	}
 
-			Vector2df getStart() const { return getMin(); }
+	float getArea() const { return (end.x - getMinX()) * (end.y - getMinY());  }
 
-			Vector2df getEnd() const { return getMax(); }
+	Vector2df getMax() const { return Vector2df(end[0], end[1]); }
 
-			Vector2df getCenter() const;
+	Vector2df getMin() const { return Vector2df(getMinX(), getMinY()); }
 
-			//std::vector< Vector2d<T> > toInnerPoints(const T divideLength) const;
+	Vector2df getStart() const { return getMin(); }
 
-			float getMaxX() const { return end[0]; }
+	Vector2df getEnd() const { return getMax(); }
 
-			float getMinX() const { return start[0]; }
+	Vector2df getCenter() const {
+		return Vector2df(
+			(getMinX() + end.x) / 2.0f,
+			(getMinY() + end.y) / 2.0f
+		);
+	}
 
-			float getMaxY() const { return end[1]; }
+	//std::vector< Vector2d<T> > toInnerPoints(const T divideLength) const;
 
-			float getMinY() const { return start[1]; }
+	float getMaxX() const { return end[0]; }
 
-			Vector2df getLength() const {
-				return Vector2df(end.x - getMinX(), end.y - getMinY());
-			}
+	float getMinX() const { return start[0]; }
 
-			bool isValid() const;
+	float getMaxY() const { return end[1]; }
 
-			bool isShirinked() const;
+	float getMinY() const { return start[1]; }
 
-			bool equals(const Box2d& rhs) const;
+	Vector2df getLength() const { return Vector2df(end.x - getMinX(), end.y - getMinY()); }
 
-			bool operator==(const Box2d& rhs) const { return equals(rhs); }
+	bool isValid() const { return (getMinX() <= end.x) && (getMinY() <= end.y); }
 
-			bool operator!=(const Box2d& rhs) const { return !equals(rhs); }
+	bool isShirinked() const { return (getMinX() == end.x) && (getMinY() == end.y); }
 
-			bool hasIntersection(const Box2d& rhs) const;
+	bool equals(const Box2d& rhs) const {
+		return
+			start == rhs.getStart() &&
+			end == rhs.getEnd();
+	}
 
-			Box2d getOverlapped(const Box2d& rhs) const;
+	bool operator==(const Box2d& rhs) const { return equals(rhs); }
 
-			//Space3d<T> toSpace() const;
+	bool operator!=(const Box2d& rhs) const { return !equals(rhs); }
 
-			std::array< float, 8 > toArray() const;
+	bool hasIntersection(const Box2d& rhs) const {
+		const auto distx = std::fabs(getCenter().x - rhs.getCenter().x);
+		const auto lx = getLength().x / 2.0f + rhs.getLength().x / 2.0f;
 
-		private:
-			Vector2df start;
-			Vector2df end;
+		const auto disty = std::fabs(getCenter().y - rhs.getCenter().y);
+		const auto ly = getLength().y / 2.0f + rhs.getLength().y / 2.0f;
+
+		return (distx < lx && disty < ly);
+	}
+
+	Box2d getOverlapped(const Box2d& rhs) const {
+		assert(hasIntersection(rhs));
+		const auto minx = std::max(this->getStart().x, rhs.getStart().x);
+		const auto miny = std::max(this->getStart().y, rhs.getStart().y);
+
+		const auto maxx = std::min(this->getEnd().x, rhs.getEnd().x);
+		const auto maxy = std::min(this->getEnd().y, rhs.getEnd().y);
+
+		const Vector2df min_(minx, miny);
+		const Vector2df max_(maxx, maxy);
+		return Box2d(min_, max_);
+	}
+
+	std::array< float, 8 > toArray() const
+	{
+		return{
+			getMinX(), getMaxY(),
+			getMinX(), getMinY(),
+			getMaxX(), getMinY(),
+			getMaxX(), getMaxY()
 		};
+	}
+
+private:
+	Vector2df start;
+	Vector2df end;
+};
 
 	}
 }
---
+++ Shape/WireFrameBuilder.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../Math/Line3d.h"
+#include "../Math/Sphere3d.h"
+
+namespace Crystal {
+	namespace Shape {
+
+class WireFrameBuilder
+{
+public:
+	//void build(const Sphere)
+
+private:
+	std::vector<Math::Line3dd> lines;
+};
+	}
+}
